76b0a371af5b22d8dc9ebb3a05da57c6dbbff0ca
Taylor Fong <taylor.fong.2012@gmail.com>
2020-06-26 13:33:16 +1000

    adding to heroku

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,4 +1,5 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show, :edit, :update]
   def index
     @cocktails = Cocktail.all
   end
@@ -14,7 +15,6 @@ class CocktailsController < ApplicationController
   end
 
   def show
-    @cocktail = Cocktail.find(params[:id])
     @dose = Dose.new
   end
 
@@ -22,9 +22,20 @@ class CocktailsController < ApplicationController
   end
 
   def update
+    @cocktail.update(cocktail_params)
+    redirect_to cocktail_path(@cocktail)
+  end
+
+   def destroy
+    @cocktail.destroy
+    redirect_to cocktails_path
   end
 
   private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
 
   def cocktail_params
     params.require(:cocktail).permit(:name)
