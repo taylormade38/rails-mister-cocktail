a6688ad99fca8c5c50acb212cec081a47709bc19
Taylor Fong <taylor.fong.2012@gmail.com>
2020-06-26 17:12:37 +1000

    added cloudinary

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -10,8 +10,11 @@ class CocktailsController < ApplicationController
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
-    @cocktail.save
-    redirect_to cocktail_path(@cocktail)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   def show
